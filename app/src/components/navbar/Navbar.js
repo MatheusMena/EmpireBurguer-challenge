bb3fdcddf26b0a9c6c7460be1b7087bd24f570e5
MatheusMena <matheusmena1@hotmail.com>
2022-09-06 20:54:00 -0300

    feat:Add icons  Navbar

--- app/src/components/navbar/Navbar.js
+++ app/src/components/navbar/Navbar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./style.css"
-import logo from'./empire.png'
+import logo from'./images/empire.png'
+import Ifood from'./images/ifood.svg'
+import Instagram from'./images/instagram.svg'
+import Contato from'./images/contato.svg'
+
 export default function Navbar() {
   return (
     <div className='navbar'>
@@ -10,6 +14,10 @@ export default function Navbar() {
       <div className='items cardapio'>Cardapio</div>
       <div className='items comentario'>Comentario</div>
       <div className='items contato'>Contato</div>
+      <img className="ifood" src={Ifood} alt="logo ifood"/>
+      <img className="instagram" src={Instagram} alt="logo Instagram"/>
+      <div className="barra"/>
+      <img className="contato" src={Contato} alt="logo Contato"/>
       </div>
  )
 }
